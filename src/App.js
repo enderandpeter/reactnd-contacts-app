41a7f6adc5b7ac95d1d29445a1af53be4291eec9
Spencer Williams <spencer@aninternetpresence.net>
2018-12-07 20:41:23 -0700

    Move contacts to state and remove package-lock for yarn

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:3001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:3001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:3001/tyler.jpg"
- }
-];
-
 class App extends Component {
+  state = {
+    contacts : [
+       {
+         "id": "karen",
+         "name": "Karen Isgrigg",
+         "handle": "karen_isgrigg",
+         "avatarURL": "http://localhost:3001/karen.jpg"
+       },
+       {
+         "id": "richard",
+         "name": "Richard Kalehoff",
+         "handle": "richardkalehoff",
+         "avatarURL": "http://localhost:3001/richard.jpg"
+       },
+       {
+         "id": "tyler",
+         "name": "Tyler McGinnis",
+         "handle": "tylermcginnis",
+         "avatarURL": "http://localhost:3001/tyler.jpg"
+       }
+    ]
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
