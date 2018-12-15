8770b0613589bb26540177b37ce294c76410273d
Spencer Williams <spencer@aninternetpresence.net>
2018-12-15 12:19:42 -0700

    Add `/create/` route and `BrowserRouter`.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
@@ -27,11 +28,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
-        <CreateContact />
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )} />
+        <Route path='/create' component={CreateContact} />
       </div>
     );
   }
